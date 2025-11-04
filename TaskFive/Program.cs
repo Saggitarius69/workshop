441f7e938c0fae2c25aa8be2c3cbe4ed83cfa203
Saggitarius69 <obitothapa2061@gmail.com>
2025-11-04 11:17:30 +0545

    Task 5 Completed

---
+++ TaskFive/Program.cs
@@ -0,0 +1,20 @@
+﻿﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        DateTime birthDate = new DateTime(2002, 11, 17);
+
+        DateTime currentDate = DateTime.Now;
+
+        TimeSpan ageSpan = currentDate - birthDate;
+        int age = ageSpan.Days / 365;
+        
+        Console.WriteLine("Birthdate: " + birthDate);
+        Console.WriteLine("Current Date: " + currentDate);
+        Console.WriteLine("Your Age: " + age + " years");
+
+
+        DateTime add10BirthDate = birthDate.AddDays(10);
+        Console.WriteLine("Birthdate plus 10 days: " + add10BirthDate);
+    }
+}
