5318e91415dc711b002ec6ee98a5f56fc92f227f
Saggitarius69 <obitothapa2061@gmail.com>
2025-11-04 11:25:16 +0545

    Task 6 Completed

---
+++ TaskSix/Program.cs
@@ -0,0 +1,34 @@
+﻿﻿public class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> fruits = new List<string>() { "BlackBerry", "Cantalope", "HoneyMelon" };
+
+        fruits.Add("Orange");
+
+        fruits.Remove("HoneyMelon");
+
+        Console.WriteLine("Fruits in the list:");
+        foreach (string fruit in fruits)
+        {
+            Console.WriteLine(fruit);
+        }
+
+        Dictionary<int, string> fruitDictionary = new Dictionary<int, string>()
+        {
+            { 1, "BlackBerry" },
+            { 2, "Cantalope" },
+            { 3, "Dragon Fruit" }
+        };
+
+        
+        fruitDictionary.Add(4, "Cranberry");
+
+       
+        Console.WriteLine("\nFruit Dictionary:");
+        foreach (var item in fruitDictionary)
+        {
+            Console.WriteLine("ID: " + item.Key + " Fruit: " + item.Value);
+        }
+    }
+}
